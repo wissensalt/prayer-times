09779dda806ca1fab8ca49f24c8d39a0587990f9
wissensalt <fauzi.knightmaster.achmad@gmail.com>
2021-06-28 20:16:39 +0700

    add screenshots

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css"
-import "./css/app.css"
 import {Background} from "./components/Background";
 import {PrayItem} from "./components/PrayItem";
 
